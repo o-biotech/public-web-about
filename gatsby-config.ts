c5d8cd3220b7a4735e50639092dda8605f059e84
Michael Gearhardt <michael.gearhardt@fathym.com>
2025-02-09 23:20:29 -0700

    Update gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,7 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  pathPrefix: "", // Add your path prefix here
+  pathPrefix: "./", // Add your path prefix here
   plugins: [{
   resolve: "@plasmicapp/loader-gatsby",
   options: {
